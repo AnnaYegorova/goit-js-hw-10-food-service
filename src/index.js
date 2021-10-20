aedd48c6757d42e413f4125ca3c7d22f6825383f
AnnaYegorova <good4anna@gmail.com>
2021-10-20 15:56:44 +0300

    создает переключатель тем

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './sass/main.scss';
+import data from "./menu.json"
+import template from "./templates/photoByArray.hbs"
+import refs from "./js/refs"
+
+const {photoList, checkboxBtn} = refs
+
+const result = template(data)
+photoList.insertAdjacentHTML("beforeend", result)
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const {LIGHT, DARK} = Theme
+
+checkboxBtn.addEventListener("change", changeTheme)
+
+function changeTheme(e){
+    const theme = e.target.checked
+    if(theme){
+        document.querySelector("body").classList.add(DARK)
+        document.querySelector("body").classList.remove(LIGHT)
+        localStorage.setItem('theme', DARK)
+        
+    } else {document.querySelector("body").classList.add(LIGHT)
+    document.querySelector("body").classList.remove(DARK)
+    localStorage.setItem('theme', LIGHT)
+  }
+}
+
+let theme = localStorage.getItem('theme')
+
+ if(!theme){
+     theme = LIGHT
+     localStorage.setItem('theme', theme)
+     } 
+     document.querySelector('body').classList.add(theme)
+     checkboxBtn.checked = theme === LIGHT ? false : true
+
+
+ 
